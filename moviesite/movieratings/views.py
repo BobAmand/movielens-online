f3300555107e4fa06397c3fa9a168f2781620035
Bob Amand <biotmg@nc.rr.com>
2015-10-09 14:39:36 -0400

    new slate

--- moviesite/movieratings/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-# need to return an http response
-# def default():
-#     return HttpResponse("this is the response")
