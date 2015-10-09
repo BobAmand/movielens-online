cefefe2443bdf50766e2aeea621b6ee53e48d6a3
Bob Amand <biotmg@nc.rr.com>
2015-10-09 15:59:19 -0400

    model file update

--- movielens/movieapp/models.py
+++ movielens/movieapp/models.py
@@ -30,3 +30,29 @@ class Rater(models.Model):   #simplified models
         return (str(self.id))
 
 class Movie(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'movies'
+
+    title = models.CharField(max_length=255, null=True)
+        # minimized the list of data available; genria is in View Git
+
+    def average_rating(self):
+        return self.rating_set.aggregate(models.Avg('stars'))['stars__avg']
+        # 'stars__avg' is dynamically calculating the average.
+    def __str__(self):
+        return self.title
+
+class Rating(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'ratings'
+
+    citizen = models.ForeignKey(Rater)
+    movie = models.ForeignKey(Movie)
+    stars = models.PositiveIntegerField()
+
+    def __str__(self):
+        return 'User {} rates {} with {} stars.'.format(self.citizen, self.movie, self.stars)
+
+        
