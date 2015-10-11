305e97901c27a00d84c88f4b0b7146b26664019c
Bob Amand <biotmg@nc.rr.com>
2015-10-11 17:30:56 -0400

    Intermediate views are working

--- movielens/movieapp/urls.py
+++ movielens/movieapp/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'movie/(?P<movie_id>\d+)', views.movie_detail),   # a movie page
+    url(r'movie/(?P<movie_id>\d+)$', views.movie_detail),   # a movie page
+    url(r'rater/(?P<rater_id>\d+)$', views.rater_detail),   # a rater page
 
 ]
--- movielens/movieapp/views.py
+++ movielens/movieapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Movie
+from .models import Movie, Rater
 
 # Create your views here.
 
@@ -8,3 +8,8 @@ def movie_detail(request, movie_id):
     movie = Movie.objects.get(pk=movie_id)    # lookup from Movie dict
     return render(request, 'movieapp/movie_detail.html',
                   {'movie': movie})          # passing the movie object in.
+
+def rater_detail(request, rater_id):
+    rater = Rater.objects.get(pk=rater_id)    # lookup from Movie dict
+    return render(request, 'movieapp/rater_detail.html',
+                  {'rater': rater})          # passing the movie object in.
