c34b246ddaff75e69a649d3f73d7c9456d7e94d1
Bob Amand <biotmg@nc.rr.com>
2015-10-11 17:00:24 -0400

    Built views of movie and ratings

--- movielens/movieapp/models.py
+++ movielens/movieapp/models.py
@@ -29,7 +29,7 @@ class Rater(models.Model):   #simplified models
     zipcode = models.CharField(max_length=5, null=True)
 
     def __str__(self):
-        return (str(self.id))
+        return str(self.id)
 
 class Movie(models.Model):
 
