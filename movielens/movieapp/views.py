a35a4684a691bd96d4ca4df30148bcb02aca2a5c
Bob Amand <biotmg@nc.rr.com>
2015-10-11 16:22:51 -0400

    Files supporting initial views

--- movielens/movieapp/views.py
+++ movielens/movieapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
+
+
+def movie_detail(request, movie_id):
+    movie = Movie.objects.get(pk=movie_id)    # lookup from Movie dict
+    return render(request, 'movieapp/movie_detail.html',
+                  {'movie': movie})          # passing the movie object in.
