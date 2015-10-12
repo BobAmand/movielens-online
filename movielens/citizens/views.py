f82e8839ba58fe5a24c43e3f042df3aef4f7e479
Bob Amand <biotmg@nc.rr.com>
2015-10-12 15:30:11 -0400

    Final commit on this branch

--- movielens/citizens/views.py
+++ movielens/citizens/views.py
@@ -1,25 +1,31 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-
-# Create your views here.
-
-def login(request):
-    if request.methon == 'POST':
-        # attempting to log in
-        username = request.POST['citizenname']
-        password = request.POST['password']
-
-        user = authenticate(username=username, password= password)
-
-        if user is not None and user.is_active:
-            logoin(request, user)
-            return redirect('movie_detail')
-        else:
-            return render(request,
-                  'citizens/login.html',
-                  {'failed': True,
-                  "username": username})
-
-    return render(request,
-                  'citizens/login.html',
-                  {})
+# from django.shortcuts import render
+# from django.contrib.auth import authenticate, login, logout
+# from xxxxxxxx
+#
+# # Create your views here.
+#
+# def login(request):
+#     if request.methon == 'POST':
+#         # attempting to log in
+#         username = request.POST['citizenname']
+#         password = request.POST['password']
+#
+#         user = authenticate(username=username, password= password)
+#
+#         if user is not None and user.is_active:
+#             logoin(request, user)
+#             return redirect('movie_detail')
+#         else:
+#             return render(request,
+#                   'citizens/login.html',
+#                   {'failed': True,
+#                   "username": username})
+#
+#     return render(request,
+#                   'citizens/login.html',
+#                   {})
+#
+#
+#
+# @login_required
+# etx.....
