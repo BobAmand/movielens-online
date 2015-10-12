3756b777b893f86695974c98612856e7a70f7633
Bob Amand <biotmg@nc.rr.com>
2015-10-11 20:50:21 -0400

    All views functioning albeit with class work input

--- movielens/movieapp/views.py
+++ movielens/movieapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.db.models import Avg, Count
 from .models import Movie, Rater
+
 
 # Create your views here.
 
@@ -19,4 +21,24 @@ def rater_detail(request, rater_id):
         })
     return render(request, 'movieapp/rater_detail.html',
                   {'rater': rater,
-                  'movie_ratings': movie_ratings}) # Added movieratings.
+                   'movie_ratings': movie_ratings})  # Added movieratings
+
+
+def top_movies(request):
+    # movie_list = []    -too many hits on database (every Database)
+    # for movie in Movie.objects.all():
+    #     if type(movie.average_rating()) == float:
+    #         movie_list.append(movie)
+    popular_movies = Movie.objects.annotate(num_ratings=Count('rating')) \
+                                  .filter(num_ratings__gte=50)
+    # more dunder magic.
+    # Advanced Database optimization from class:
+    movies = popular_movies.annotate(Avg('rating__stars')) \
+                           .order_by('-rating__stars__avg')[:20]
+
+    #  annotate, rating__stars, and -rating__stars__avg [directly from notes]
+    #  return self.rating_set.aggregate(models.Avg('stars'))['stars__avg']
+    # 'stars__avg' is dynamically calculating the average.
+
+    return render(request,
+                  'movieapp/top_movies.html', {'movies': movies})
